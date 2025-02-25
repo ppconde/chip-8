c7b4d7fbdb3363e97ac6041c103cc65f9269b07e
Pedro Conde <pepconde.1993@gmail.com>
2025-02-25 23:51:32 +0000

    fix rom path

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use minifb::{Window, WindowOptions};
 
 fn main() {
     let mut chip8 = chip8::Chip8::new();
-    chip8.load_rom("game.ch8").unwrap();
-
+    chip8.load_rom("roms/glitchGhost.ch8").unwrap();
     let mut window = Window::new("CHIP-8 Emulator", 64, 32, WindowOptions::default()).unwrap();
 }
